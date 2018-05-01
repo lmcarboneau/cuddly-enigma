74718228c31df22c350691541de4367cc8c15d08
Lindsey Carboneau <lmcarboneau@users.noreply.github.com>
2018-05-01 11:13:43 -0400

    fix image lists

--- theServer.py
+++ theServer.py
@@ -29,8 +29,8 @@ class ThisHandler(BaseHTTPRequestHandler):
       imagefiles = []
       for filename in os.listdir(os.getcwd()):
         if fnmatch.fnmatch(filename, '*.jpg'):
-          imagefiles.append('<a href="uas-at-fgcu.com/' + filename + '"></a>')
-      imagefiles.sort()
+          imagefiles.append('<a href="uas-at-fgcu.com/' + filename + '"></a><br>')
+      join(imagefiles.sort(), ' ')
       self.wfile.write('!DOCTYPE html>   <html lang="en">    <title>UAS at FGCU </title>    <meta name="viewport" content="width=device-width, initial-scale=1">    <link rel="stylesheet" href="https://unpkg.com/tachyons/css/tachyons.min.css">    <body>     <header class="bg-black-90 fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">      <nav class="f6 fw6 ttu tracked">       <a class="link dim white dib mr3" href="http://arduino.fgcu.edu/" title="Home">Arduino at FGCU Home</a>      </nav>     </header>      <section class="flex-ns vh-100 items-center">        '
 		      + imagefiles + '<a class="f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib black" href="uas-at-fgcu.com/images">           View Images         </a>        </div>      </section>     <footer class="pv4 ph3 ph5-m ph6-l mid-gray">      <small class="f6 db tc">Ã‚Â© 2018 <b class="ttu">Software Engineering at Florida Gulf Coast University</b>., All Rights Reserved</small>     </footer>    </body>  </html>    ')
     if self.path in paths:
