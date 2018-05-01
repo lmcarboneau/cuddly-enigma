5c57c222d7c33e0628ddc820ae8f6c957b17cc49
Ubuntu <ubuntu@ip-172-26-2-52.us-east-2.compute.internal>
2018-05-01 04:46:01 +0000

    needs the server if/when something breaks

---
+++ theServer.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Very simple HTTP server in python for requests
+Usage::
+	./theServer.py
+"""
+
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HOST_NAME = ''
+PORT_NUMBER = 80
+
+class ThisHandler(BaseHTTPRequestHandler):
+  def do_HEAD(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'application/json; charset=utf-8')
+    self.end_headers()
+
+  def do_GET(self):
+    paths = {
+      '/status': {'status':200}
+    }
+
+    if self.path in paths:
+      self.respond(paths[self.path])
+    else:
+      self.respond({'status':500})
+
+  def do_PUT(self):
+    content_length = int(self.headers['Content-length'])
+    post_data = self.rfile.read(content_length)
+
+    self.respond({'status':503, 'content': post_data})
+
+  def handle_http(self, status_code, content = ''):
+    self.send_response(status_code)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+    content = '{}'.format(content)
+    return bytes(content, 'UTF-8')
+
+  def respond(self, opts):
+    if 'content' in opts:
+      response = self.handle_http(opts['status'], opts['content'])
+    else:
+      response = self.handle_http(opts['status'])
+    self.wfile.write(response)
+
+if __name__ == '__main__':
+  server_class = HTTPServer
+  httpd = server_class((HOST_NAME, PORT_NUMBER), ThisHandler)
+  print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+  try:
+    httpd.serve_forever()
+  except KeyboardInterrupt:
+    pass
+  httpd.server_close()
+  print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
+
